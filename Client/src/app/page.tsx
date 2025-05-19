c1f8213194a8d663ba728d9e8230d3b5934abe43
minhdang0911 <121071062+minhdang0911@users.noreply.github.com>
2025-05-19 10:31:34 +0700

    init project

---
+++ Client/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>
+      project
+      <div>
+        <Button type="primary">init</Button>
+      </div>
+    </div>
+  );
+}
