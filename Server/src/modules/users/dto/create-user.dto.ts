4a4b1f9ad97fa5f592090cef3c2b1cf05fc1e0a5
minhdang0911 <121071062+minhdang0911@users.noreply.github.com>
2025-05-20 10:40:13 +0700

    Build api create/getAll with Query Users

--- Server/src/modules/users/dto/create-user.dto.ts
+++ Server/src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { IsEmail, isNotEmpty, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'Họ tên không được để trống' })
+  name: string;
+  @IsNotEmpty({ message: 'Email không được để trống' })
+  @IsEmail({}, { message: 'email không đúng định dạng' })
+  email: string;
+  @IsNotEmpty({ message: 'Mật khẩu không được để trống' })
+  password: string;
+  phone: string;
+  address: string;
+  image: string;
+}
--- Server/src/modules/users/schemas/user.schema.ts
+++ Server/src/modules/users/schemas/user.schema.ts
@@ -1,3 +1,4 @@
+import { IsEmpty } from 'class-validator';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 
@@ -5,39 +6,38 @@ export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
 export class User {
-    @Prop()
-    name: string;
+  @IsEmpty()
+  name: string;
 
-    @Prop()
-    email: string;
+  @Prop()
+  email: string;
 
-    @Prop()
-    password: string;
+  @Prop()
+  password: string;
 
-    @Prop()
-    phone: string;
+  @Prop()
+  phone: string;
 
-    @Prop()
-    address: string;
+  @Prop()
+  address: string;
 
-    @Prop()
-    image: string;
+  @Prop()
+  image: string;
 
-    @Prop()
-    role: string;
+  @Prop({ default: 'USERS' })
+  role: string;
 
-    @Prop()
-    accountType: string;
+  @Prop({ default: 'LOCAL' })
+  accountType: string;
 
-    @Prop()
-    isActive: string;
+  @Prop({ default: false })
+  isActive: boolean;
 
-    @Prop()
-    codeId: string;
+  @Prop()
+  codeId: string;
 
-    @Prop()
-    codeExpired: Date;
-
+  @Prop()
+  codeExpired: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
